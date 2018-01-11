433f2b060cdf395d2dcbd6ef7e1e3983c4b5c7ae
Matthew Wells <mdub329@gmail.com>
2018-01-11 15:36:56 -0600

    Initial Commit

---
+++ Concession Inventory Tracker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Concession Inventory Tracker
+//
+//  Created by Matthew Wells on 1/11/18.
+//  Copyright © 2018 Matthew Wells. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
