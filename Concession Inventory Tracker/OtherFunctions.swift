9894efd658a756246a7d91c556a4c528013ec60f
Matthew Wells <MDub329@gmail.com>
2018-01-19 18:40:34 -0600

    Add OtherFunc File

---
+++ Concession Inventory Tracker/OtherFunctions.swift
@@ -0,0 +1,9 @@
+//
+//  OtherFunctions.swift
+//  Concession Inventory Tracker
+//
+//  Created by Matthew Wells on 1/19/18.
+//  Copyright © 2018 Matthew Wells. All rights reserved.
+//
+
+import Foundation
